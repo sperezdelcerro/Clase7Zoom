ee13ce88370520742434c24c1bc8fdf8b7003c69
sperezdelcerro <sperezdelcerro@icloud.com>
2021-06-22 19:40:39 -0300

    sacamos este error y termina

--- sere 4.js
+++ sere 4.js
@@ -1,6 +1,4 @@
 let punto9 = "estamos terminando";
-<<<<<<< HEAD
+
 let sere4 = "dale que ya casii";
-=======
-let casi = "ultima prueba de git";
->>>>>>> f33065de7cca95aceea81acc31fbc1dbffaf3903
+let casi = "ultima prueba de git"; 
--- yaTermina.js
+++ yaTermina.js
@@ -1,6 +1,4 @@
 let clari4 = "tengo que terminar de hacer la autoevaluación";
-<<<<<<< HEAD
 let sere5 = "yo la termine aunque me copie los ultimos dos";
-=======
 let ya = "terminamos con git";
->>>>>>> f33065de7cca95aceea81acc31fbc1dbffaf3903
+
