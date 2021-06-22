02db520ea5f78ab8bd1fe4d66d96b5d780717195
sperezdelcerro <sperezdelcerro@icloud.com>
2021-06-22 19:31:10 -0300

    PUNTO FINAÃL

--- sere 4.js
+++ sere 4.js
@@ -1,1 +1,2 @@
 let punto9 = "estamos terminando";
+let sere4 = "dale que ya casii";
--- yaTermina.js
+++ yaTermina.js
@@ -1,1 +1,2 @@
 let clari4 = "tengo que terminar de hacer la autoevaluación";
+let sere5 = "yo la termine aunque me copie los ultimos dos";
